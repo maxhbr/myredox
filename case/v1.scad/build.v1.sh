3986ffe34942397d4054e7158603dba5a6e95b0d
Maximilian Huber <gh@maxhbr.de>
2022-04-01 06:15:27 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- case/v1.scad/build.v1.sh
+++ case/v1.scad/build.v1.sh
@@ -4,29 +4,43 @@
 set -euo pipefail
 
 buildConfig() {
-    local stl="$(echo "$1" | cut -d':' -f1)"
-    local part="$(echo "$1" | cut -d':' -f2)"
+    if [[ "$1" ]]; then
+        local stl="$(echo "$1" | cut -d':' -f1)"
+        local part="$(echo "$1" | cut -d':' -f2)"
 
-    (set -x;
-     openscad --hardwarnings -o "$stl" -p "v1.configs.json" -P "$part" v1.scad
-    )
+        mkdir -p "$(dirname "$stl")"
+
+        (set -x;
+        openscad --hardwarnings -o "$stl" -p "v1.configs.json" -P "$part" v1.scad
+        )
+    fi
 }
 export -f buildConfig
 
-mkdir -p out
-mkdir -p out/wip
-
-cat <<EOF | parallel --progress buildConfig {} 1>&2
+buildCases() {
+    cat <<EOF
 out/case/redox.v1.left.stl:case
 out/case/redox.v1.right.stl:case_right
 out/caseWithLipo/redox.v1.left.lipo.stl:caseWithLipo
 out/caseWithLipo/redox.v1.right.lipo.stl:caseWithLipo_right
 out/caseWithPrintedPlate/redox.v1.left.printedPlate.stl:caseWithPrintedPlate
 out/caseWithPrintedPlate/redox.v1.right.printedPlate.stl:caseWithPrintedPlate_right
-out/tentKit20.stl:tentKit20
-out/tentKit30.stl:tentKit30
-out/tentKit40.stl:tentKit40
-out/tentKit50.stl:tentKit50
+EOF
+}
+
+buildTentKits() {
+    return 0
+    cat <<EOF
+out/tentKits/tentKit20.stl:tentKit20
+out/tentKits/tentKit30.stl:tentKit30
+out/tentKits/tentKit40.stl:tentKit40
+out/tentKits/tentKit70.stl:tentKit70
+EOF
+}
+
+cat <<EOF | parallel --progress buildConfig {} 1>&2
+$(buildCases)
+$(buildTentKits)
 EOF
 
 wait
